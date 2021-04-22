facb8cd2d94a9ffe7486250102363c0db9bdc62b
HARSHENI <harsheni.chandran@augustahitech.com>
2021-04-22 20:58:27 +0530

    title changed

--- app.js
+++ app.js
@@ -40,7 +40,7 @@ app.use(express.static(path.join(__dirname, "public")));
 // Swagger definition
 var swaggerDefinition = {
     info: {
-        title: "Recruiting Candidates",
+        title: "Hospital Management System",
         version: "1.0.0",
         description: "Hospital CRUD using Swagger API and MongoDB",
     },
